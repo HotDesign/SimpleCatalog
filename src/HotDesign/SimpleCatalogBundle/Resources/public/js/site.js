caab62f17e07eedd3df82f48c7f86725217e992c
Marian0 <marianosantafe@gmail.com>
2012-02-17 19:23:47 -0300

    Se termino de arreglar el edit del scGeoExt

--- src/HotDesign/SimpleCatalogBundle/Resources/public/js/site.js
+++ src/HotDesign/SimpleCatalogBundle/Resources/public/js/site.js
@@ -1,16 +1,22 @@
 $('a').tooltip();
- 
- 
-$("a[rel^='prettyPhoto']").prettyPhoto();
-				
-$(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({
-    animation_speed:'normal',
-    theme:'light_square',
-    slideshow:3000, 
-    autoplay_slideshow: true
-});
-$(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({
+	
+// Lighbox        
+$("a[rel^='prettyPhoto']").prettyPhoto({
     animation_speed:'fast',
-    slideshow:10000, 
-    hideflash: true
+    theme:'light_rounded',
+    slideshow: false, 
+    autoplay_slideshow: false,
+    social_tools: ' ' 
 });
+
+$(".scactions a[rel^='prettyPhoto']").prettyPhoto({
+    animation_speed:'fast',
+    theme:'light_rounded',
+    slideshow: false, 
+    autoplay_slideshow: false,
+    social_tools: ' ',    
+    callback: function(){
+          location.reload();
+    }
+});
+	

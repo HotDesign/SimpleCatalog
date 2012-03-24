310646262a53c3bf47806ad6e1a062674b5920f9
Germanaz0 <germanazo@gmail.com>
2012-03-24 14:27:10 -0300

    Added validators in server side to contact form

--- src/HotDesign/ScThemeBundle/Form/ScContactFormType.php
+++ src/HotDesign/ScThemeBundle/Form/ScContactFormType.php
@@ -4,7 +4,9 @@ namespace HotDesign\ScThemeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
-
+use Symfony\Component\Validator\Constraints\Collection;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 /**
  * Description of ScContactFormType
  *
@@ -15,7 +17,37 @@ class ScContactFormType extends AbstractType {
     public function buildForm(FormBuilder $builder, array $options) { 
         $builder->add('name', 'text', array('label' => 'Nombre'));
         $builder->add('email', 'email', array('label' => 'E-Mail'));
+        $builder->add('subject', 'text', array('label' => 'Asunto'));
         $builder->add('description', 'textarea', array('label' => 'Mensaje'));
+    }
+    
+        /**
+     * @param  array $options
+     * @return array
+     */
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'validation_constraint' => new Collection(array(
+                'name'  => new NotBlank(array(
+                    'message' => 'contact_form.name.blank'
+                )),
+                'email' => array(
+                    new NotBlank(array(
+                        'message' => 'contact_form.email.blank'
+                    )),
+                    new Email(array(
+                        'message' => 'contact_form.email.invalid'
+                    ))
+                ),
+                
+                'subject' => array(),
+                
+                'description' => new NotBlank(array(
+                    'message' => 'contact_form.description.blank'
+                )),
+            ))
+        );
     }
     
    public function getName() {

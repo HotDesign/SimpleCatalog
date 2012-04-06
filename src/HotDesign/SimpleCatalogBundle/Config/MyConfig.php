51f1dd7b0ab947b361d2aecff001483d946bd0db
Marian0 <marianosantafe@gmail.com>
2012-04-06 13:04:39 -0300

    Added config path to upload images. Wokrs on backend. Repair on frontend.

--- src/HotDesign/SimpleCatalogBundle/Config/MyConfig.php
+++ src/HotDesign/SimpleCatalogBundle/Config/MyConfig.php
@@ -7,11 +7,15 @@ namespace HotDesign\SimpleCatalogBundle\Config;
  * 
  */
 class MyConfig {
-    public static $default_lat = -31.632301;
+	public static $default_lat = -31.632301;
     public static $default_lng = -60.716157;    
   
     public static $items_per_pages = 10;
     public static $image_max_size_bytes  = 10485760;  //bytes
 
     public static $max_image_per_item = 5;  
+    
+
+    public static $sitename = 'SimpleCatalog';
+    public static $image_upload_path = 'catalog/images/';  
 }

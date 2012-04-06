1d5b6e96c1c56658f3e02f6fc8ba3f12d2d87c91
Germanaz0 <germanazo@gmail.com>
2012-04-06 14:05:13 -0300

    Se acomodo el config y se agregaron comentarios

--- src/HotDesign/SimpleCatalogBundle/Config/MyConfig.php
+++ src/HotDesign/SimpleCatalogBundle/Config/MyConfig.php
@@ -7,19 +7,22 @@ namespace HotDesign\SimpleCatalogBundle\Config;
  * 
  */
 class MyConfig {
+    //Site configuration
+    public static $sitename = 'SimpleCatalog';    
+    
+    // Geographic configurations
     public static $default_lat = -31.632301;
     public static $default_lng = -60.716157;    
   
+    //Pagination configurations
     public static $items_per_pages = 10;
-    public static $items_per_page_backend = 10;
+    public static $items_per_page_backend = 20;
 
+    //Image validations
     public static $image_max_size_bytes  = 10485760;  //bytes
-
-
     public static $max_image_per_item = 5;
-  
 
-    public static $sitename = 'SimpleCatalog';
+    //Image upload assets
     public static $image_upload_path = 'catalog/images/';  
 
 }
